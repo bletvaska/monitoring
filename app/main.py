d775f951874d68ed49f6dded791f646c6ea40cd5
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-05-12 11:19:22 +0200

    some log messages

--- app/main.py
+++ app/main.py
@@ -8,17 +8,21 @@ username = 'john'
 password = 'hello.world'
 
 logger = logging.getLogger('dummy')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s [%(name)s]: %(message)s'
+)
 
 @app.get("/api/hello")
 def hello_world():
-    logger.debug('>> inside of hello_world()')
-    logger.debug(f'>> username is {username} and password {password}')
+    logger.debug('inside of hello_world()')
+    logger.debug(f'username is {username} and password {password}')
 
-    logger.debug('>> debug')
-    logger.info('>> info')
-    logger.warning('>> warning')
-    logger.error('>> error')
-    logger.critical('>> critical')
+    logger.debug('debug')
+    logger.info('info')
+    logger.warning('warning')
+    logger.error('error')
+    logger.critical('critical')
 
     return {"Hello": "World"}
 
