ba7b9a58a9eb291df4086b9259023d02f08fd565
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-05-12 11:14:47 +0200

    some log messages

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
+import logging
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+username = 'john'
+password = 'hello.world'
 
-@app.get("/")
-def read_root():
+logger = logging.getLogger('dummy')
+
+@app.get("/api/hello")
+def hello_world():
+    logger.debug('>> inside of hello_world()')
+    logger.debug(f'>> username is {username} and password {password}')
+
+    logger.debug('>> debug')
+    logger.info('>> info')
+    logger.warning('>> warning')
+    logger.error('>> error')
+    logger.critical('>> critical')
+
     return {"Hello": "World"}
 
