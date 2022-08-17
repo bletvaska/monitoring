696c83754201713b648ad6ae2ee0873983c1ca84
Ubuntu <ubuntu@ip-172-31-36-3.eu-central-1.compute.internal>
2022-08-17 15:03:21 +0200

    added exception endpoint

--- app/main.py
+++ app/main.py
@@ -48,3 +48,32 @@ def get_time(request: Request):
         'unixtime': int(time())
     }
 
+
+
+@app.get('/api/exception')
+def exception_example():
+    try:
+        print('>> start')
+
+        file = open('/root/doesnt.exist', 'r')
+
+        10 / 0
+
+        print('>> end')
+
+    except ZeroDivisionError as ex:
+        logger.error('>> it is not possible to divide by zero')
+        logger.exception(ex)
+
+    except FileNotFoundError as ex:
+        logger.error('Error: File was not found')
+        logger.exception(ex)
+
+    except PermissionError as ex:
+        logger.error('Error: You dont have permissions to access this file')
+        logger.exception(ex)
+
+    except Exception as ex:
+        # never happens
+        pass
+
