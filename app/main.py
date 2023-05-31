f7b542a94a55cc7e6caa1d08eb250436bca685b8
Ubuntu <mirek@cnl.sk>
2023-05-31 14:35:04 +0200

    addded support for loki

--- app/main.py
+++ app/main.py
@@ -2,10 +2,21 @@ import logging
 
 import pendulum
 from fastapi import FastAPI
+import logging_loki
 
+# create loki handler
+handler = logging_loki.LokiHandler(
+    url="http://loki:3100/loki/api/v1/push",
+    version='1'
+)
+
+# configure logging
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(name)s:%(lineno)d %(levelname)-8s: %(message)s")
+
+# create logger
 logger = logging.getLogger(__name__)
+logger.addHandler(handler)
 
 logger.info('Starting WorldTime Application.')
 app = FastAPI()
