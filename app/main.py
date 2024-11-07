cff2758a29ec3be9af7dff3f663512fa9fdd9b76
mirek <mirek@cnl.sk>
2024-11-07 13:52:33 +0100

    added process time middleware

--- app/main.py
+++ app/main.py
@@ -50,15 +50,24 @@ def log_client_ip(func):
     return wrapper
 
 
+@app.middleware('http')
+async def add_process_time_header(request: Request, call_next):
+    start = pendulum.now()
+    response = await call_next(request)
+    duration = pendulum.now() - start
+    response.headers['X-Process-Time'] = f'{duration.microseconds}'
+    return response
+
+
 @app.get('/api/timezones')
 @log_client_ip
 async def get_list_of_timezones(request: Request):
     return pendulum.timezones()
 
+
 @app.get('/api/timezones/{area}/{location}')
 @log_client_ip
 async def get_time_from_timezone(request: Request, area, location):
-    # logger.info(f'Connection from {request.client.host}')
     try:
         now = pendulum.now(f'{area}/{location}')
         return now.isoformat()
