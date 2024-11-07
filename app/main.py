bc0bb61fda41f72230100e67ab9b8ba25554e90a
mirek <mirek@cnl.sk>
2024-11-07 14:21:39 +0100

    adding prometheus - not tested yet

--- app/main.py
+++ app/main.py
@@ -55,7 +55,7 @@ async def add_process_time_header(request: Request, call_next):
     start = pendulum.now()
     response = await call_next(request)
     duration = pendulum.now() - start
-    response.headers['X-Process-Time'] = f'{duration.microseconds}'
+    response.headers['X-Process-Time'] = f'{duration.as_duration()}'
     return response
 
 
